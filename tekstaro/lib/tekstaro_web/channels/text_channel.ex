0fc9a00f3bd88b2743278e652494021abc28b357
Gordon Guthrie <gordon@hypernumbers.com>
2020-03-12 16:25:21 +0100

    remove IO.inspect statements

--- tekstaro/lib/tekstaro_web/channels/text_channel.ex
+++ tekstaro/lib/tekstaro_web/channels/text_channel.ex
@@ -4,15 +4,12 @@ defmodule TekstaroWeb.TextChannel do
   intercept ["status"]
 
   def join(name, _params, socket) do
-  	IO.inspect(name, label: "got hash in channel")
   	reply = Tekstaro.Text.Text.get_status(name)
     {:ok, reply, socket}
   end
-  
+
  def handle_out("status", msg, socket) do
-	IO.inspect(msg, label: "being handled out")
-    ret = push(socket, "status", msg)
-    IO.inspect(ret, label: "push to socket returns")
+    :ok = push(socket, "status", msg)
     {:noreply, socket}
   end
 
