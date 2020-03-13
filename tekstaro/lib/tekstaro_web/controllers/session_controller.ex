062a0684a633324b81b735a66df745394c025687
Gordon Guthrie <gordon@hypernumbers.com>
2020-03-13 11:35:16 +0100

    smol formatting

--- tekstaro/lib/tekstaro_web/controllers/session_controller.ex
+++ tekstaro/lib/tekstaro_web/controllers/session_controller.ex
@@ -1,5 +1,4 @@
 defmodule TekstaroWeb.SessionController do
-
   use TekstaroWeb, :controller
 
   alias Tekstaro.Accounts
@@ -10,23 +9,25 @@ defmodule TekstaroWeb.SessionController do
 
   def login(conn, %{"session" => auth_params}) do
     user = Accounts.get_by_username(auth_params["username"])
+
     case Comeonin.Bcrypt.check_pass(user, auth_params["password"]) do
       {:ok, user} ->
         conn
         |> put_session(:current_user_id, user.id)
         |> put_flash(:info, gettext("Signed in successfully"))
         |> redirect(to: "/")
+
       {:error, _e} ->
         conn
         |> put_flash(:error, gettext("there was a problem with your username/password"))
-        |>render("new.html")
-      end
+        |> render("new.html")
     end
+  end
 
-    def logout(conn, _params) do
-      conn
-      |> delete_session(:current_user_id)
-      |> put_flash("info", gettext("Signed out sucessfully"))
-      |> redirect(to: "/")
-    end
+  def logout(conn, _params) do
+    conn
+    |> delete_session(:current_user_id)
+    |> put_flash("info", gettext("Signed out sucessfully"))
+    |> redirect(to: "/")
+  end
 end
