622221e6ca3e80839366eefc6af8721618e14e05
Gordon Guthrie <gordon@hypernumbers.com>
2020-03-13 11:37:15 +0100

    smol formatting

--- tekstaro/lib/tekstaro_web/views/upload_view.ex
+++ tekstaro/lib/tekstaro_web/views/upload_view.ex
@@ -2,14 +2,14 @@ defmodule TekstaroWeb.UploadView do
   use TekstaroWeb, :view
 
   def render("upload.json", %{channel: channel}) do
-      %{
-        channel: channel
-      }
-  end
-  def render("fail.json", %{msg: msg}) do
-      %{
-        error: msg
-      }
+    %{
+      channel: channel
+    }
   end
 
+  def render("fail.json", %{msg: msg}) do
+    %{
+      error: msg
+    }
+  end
 end
