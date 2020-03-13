311eaccbe45421deb101120f296bd3ce2d4580b5
Gordon Guthrie <gordon@hypernumbers.com>
2020-03-13 11:36:44 +0100

    smol formatting

--- tekstaro/lib/tekstaro_web/controllers/upload_controller.ex
+++ tekstaro/lib/tekstaro_web/controllers/upload_controller.ex
@@ -11,40 +11,45 @@ defmodule TekstaroWeb.UploadController do
     user_id = get_session(conn, :current_user_id)
     current_user = Tekstaro.Accounts.get_user!(user_id)
     site = get_site(url)
+
     case is_input_valid(title, site) do
-        true  -> {:ok, s} = site
-                 channel = Tekstaro.Text.Text.start_child(text, title, url, s, current_user)
-                 render(conn, "upload.json", channel: channel)
-        false -> conn
-                 |> put_status(403)
-                 |> render("fail.json", msg: "please provide a title and a valid url")
+      true ->
+        {:ok, s} = site
+        channel = Tekstaro.Text.Text.start_child(text, title, url, s, current_user)
+        render(conn, "upload.json", channel: channel)
+
+      false ->
+        conn
+        |> put_status(403)
+        |> render("fail.json", msg: "please provide a title and a valid url")
     end
   end
 
   defp check_auth(conn, _params) do
-      if user_id = get_session(conn, :current_user_id) do
-        current_user = Tekstaro.Accounts.get_user!(user_id)
-        conn
-        |> assign(:current_user, current_user)
-      else
-        conn
-        |> put_flash(:error, "You need to be signed in to access that page.")
-        |> redirect(to: "/")
-        |> halt()
-      end
+    if user_id = get_session(conn, :current_user_id) do
+      current_user = Tekstaro.Accounts.get_user!(user_id)
+
+      conn
+      |> assign(:current_user, current_user)
+    else
+      conn
+      |> put_flash(:error, "You need to be signed in to access that page.")
+      |> redirect(to: "/")
+      |> halt()
     end
+  end
 
-    defp is_input_valid("", _),          do: false
-    defp is_input_valid(_, {:error, _}), do: false
-    defp is_input_valid(_, _),           do: true
+  defp is_input_valid("", _),          do: false
+  defp is_input_valid(_, {:error, _}), do: false
+  defp is_input_valid(_, _),           do: true
 
-    defp get_site(url) do
-      uri = URI.parse(url)
-      case uri do
-        %URI{scheme: nil,    host: nil}  -> {:error, "not a url"}
-        %URI{scheme: nil,    host: host} -> {:ok,    "http://" <> host}
-        %URI{scheme: scheme, host: host} -> {:ok,    scheme <> "://" <> host}
-      end
+  defp get_site(url) do
+    uri = URI.parse(url)
+
+    case uri do
+      %URI{scheme: nil,    host: nil}  -> {:error, "not a url"}
+      %URI{scheme: nil,    host: host} -> {:ok,    "http://" <> host}
+      %URI{scheme: scheme, host: host} -> {:ok,     scheme <> "://" <> host}
     end
-
+  end
 end
