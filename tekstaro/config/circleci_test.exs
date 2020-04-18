cf41165c6c916298e918089f82c01c44b9eba132
Gordon Guthrie <gordon@hypernumbers.com>
2020-04-18 13:14:07 +0200

    break out test environment config for circleci into its own file

---
+++ tekstaro/config/circleci_test.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# Configure your database
+config :tekstaro, Tekstaro.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "tekstaro_dev",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :tekstaro, TekstaroWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
